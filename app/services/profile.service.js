4dba23b52d493bc9b2e358e346b3b75668f4b6b2
Maxim Lee <mlee@key-g.com>
2017-08-18 19:43:24 +0300

    user info workaround

--- app/services/profile.service.js
+++ app/services/profile.service.js
@@ -1,35 +1,37 @@
 export default class ProfileService {
-    /* @ngInject */
-    constructor($storage) {
-      this.$storage = $storage;
-    }
+  /* @ngInject */
+  constructor($storage) {
+    this.$storage = $storage;
+  }
 
-    setProfile(data) {
-      try {
-        this.$storage.set('profile', data);
-      } catch (e) {
-        console.error("Profile don't save");
-      }
+  setProfile(data) {
+    try {
+      this.$storage.set('profile', data);
+    } catch (e) {
+      console.error("Profile don't save");
     }
+  }
 
-    getProfile() {
-      try {
-        return this.$storage.get('profile');
-      } catch (e) {
-        console.error("Profile don't load");
-      }
+  getProfile() {
+    try {
+      return this.$storage.get('profile');
+    } catch (e) {
+      console.error("Profile don't load");
     }
+  }
 
-    clearProfile() {
-      this.$storage.del('profile');
-    }
+  clearProfile() {
+    this.$storage.del('profile');
+  }
 
-    userInfo(){
-      let user = this.getProfile().user_info;
-      return {
-        id: user.id,
-        role: user.role ,
-        canSee: (category) => { return (user.permissions[category] ? true : false); },
-        };
-    }
+  userInfo() {
+    let user = this.getProfile().user_info || {};
+    return {
+      id: user.id,
+      role: user.role,
+      canSee: (category) => {
+        return (user.permissions[category] ? true : false);
+      },
+    };
+  }
 }
