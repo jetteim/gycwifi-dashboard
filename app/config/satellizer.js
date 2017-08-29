e264658ef9cd4a233a6b997e095d6c36f663fd2d
Maxim Lee <mlee@key-g.com>
2017-08-29 22:19:13 +0300

    Update satellizer.js

--- app/config/satellizer.js
+++ app/config/satellizer.js
@@ -22,6 +22,7 @@ export default ($authProvider, $envProvider) => {
   $authProvider.twitter({
     url: apiUrl + '/auth/twitter',
     clientId: '745965151480913920',
+    redirectUri: 'dashboard',
     // authorizationEndpoint: 'https://api.twitter.com/oauth/authorize',
     redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host
   });
